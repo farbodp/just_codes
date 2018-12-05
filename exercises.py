bb725c24c6a7e3c7d0e5d0f5dcf00e9cb64ddbe7
farbod <farbod.parvin72@gmail.com>
2018-12-05 17:16:10 +0330

    first commit for class exercises

---
+++ exercises.py
@@ -0,0 +1,32 @@
+# MixUp
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+def mix_up(a, b):
+  # +++your code here+++
+  return
+
+
+# donuts
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
+def donuts(count):
+  # +++your code here+++
+  return
+
+
+# both_ends
+# Given a string s, return a string made of the first 2
+# and the last 2 chars of the original string,
+# so 'spring' yields 'spng'. However, if the string length
+# is less than 2, return instead the empty string.
+def both_ends(s):
+  # +++your code here+++
+  return

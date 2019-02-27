44c70e1869cf5fd74c9d8295d72ce2fe356cbfff
Farbod Parvin <42429684+farbodp@users.noreply.github.com>
2019-02-27 19:22:56 +0330

    Add files via upload

---
+++ names.py
@@ -0,0 +1,18 @@
+
+"""Baby Names exercise
+
+Here's what the html looks like in the baby.html files:
+...
+<h3 align="center">Popularity in 1990</h3>
+....
+<tr align="right"><td>1</td><td>Michael</td><td>Jessica</td>
+<tr align="right"><td>2</td><td>Christopher</td><td>Ashley</td>
+<tr align="right"><td>3</td><td>Matthew</td><td>Brittany</td>
+...
+
+Suggested milestones for incremental development:
+ -Extract the year and print it
+ -Extract the names and rank numbers and just print them
+ -Get the names data into a dict and print it
+ -Build the [year, 'name rank', ... ] list and print it
+"""

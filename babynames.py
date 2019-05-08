4bf5c5d340239c382746583457b71b03839d6307
Farbod Parvin <42429684+farbodp@users.noreply.github.com>
2019-05-08 22:21:37 +0430

    Update babynames.py

--- babynames.py
+++ babynames.py
@@ -1,12 +1,3 @@
-
-# Copyright 2010 Google Inc.
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
-import sys
-import re
-
 """Baby Names exercise
 
 Define the extract_names() function below and change main()
@@ -29,35 +20,3 @@ Suggested milestones for incremental development:
  -Build the [year, 'name rank', ... ] list and print it
  -Fix main() to use the extract_names list
 """
-
-def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  return
-
-
-def main():
-  # This command-line parsing code is provided.
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
-  args = sys.argv[1:]
-
-  if not args:
-    print ('usage: [--summaryfile] file [file ...]')
-    sys.exit(1)
-
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
-
-  # +++your code here+++
-  # For each filename, get the names, then either print the text output
-  # or write it to a summary file
-  
-if __name__ == '__main__':
-  main()
